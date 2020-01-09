431fbef368664728d9309ab6848a9e031c7bf69f
Harley Bramwell <harleybramwell97@gmail.com>
2020-01-09 10:20:29 +0000

    Homework corrections

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -1,35 +1,28 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
-stops.push("Edinburgh Waverley")
-
-stops.unshift("Glasgow Queen St")
-
-stops.insert(4, "Polmont")
-
-#puts stops[5]
-
-stops.delete_at(2)
-
-#puts stops.count
-
-#return stops[2]
-
-#puts stops.reverse
-
-def station_printer(array)
- for stations in array
-  p stations
- end
-end
-
 
 #1. Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
 #4. Print out the index position of "Linlithgow"
+stops.index("Linlithgow")
 #5. Remove "Livingston" from the array using its name
+stops.delete("Livingston")
 #6. Delete "Cumbernauld" from the array by index
+stops.delete(2)
 #7. Print the number of stops there are in the array?
+stops.count
 #8. Show as many ways as you can to return "Falkirk High" from the array?
+stops[2]
+stops[-5]
+stops.at(2)
+stops.fetch(2)
 #9. Reverse the positions of the stops in the array
+stops.reverse
 #10 Print out all the stops using a for loop
+for stop in stops
+  p stop
+end
